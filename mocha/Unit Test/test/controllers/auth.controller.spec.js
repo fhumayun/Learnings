9cb4596c2bba290b3952d86e75bc31873b3c1628
Faisal Humayun <faisal.humayun@outlook.com>
2020-03-11 19:39:04 -0400

    Testing JavaScript for Node.js with Mocha - PluralSight

---
+++ mocha/Unit Test/test/controllers/auth.controller.spec.js
@@ -0,0 +1,37 @@
+var assert = require('assert');
+var authController = require('../../controllers/auth.controllers');
+
+
+
+describe('AuthController', function () {
+    beforeEach(function settingUpRoles() {
+        console.log('running before each');
+        authController.setRoles(['user']);
+    });
+
+    describe('isAuthorized', function () {
+
+        it('Should return false if not authorized', function () {
+            assert.equal(false, authController.isAuthorized('admin'));
+        })
+        it('Should return true if authorized', function () {
+            authController.setRoles(['user', 'admin']);
+            assert.equal(true, authController.isAuthorized('admin'));
+        })
+        this.skip()
+        it('should not allow a get if not authorized');
+        it('should allow get if authorized');
+    })
+    describe('isAuthorizedAsync', function () {
+
+        it('Should return false if not authorized', function (done) {
+            authController.isAuthorizedAsync('admin',
+                function (isAuth) {
+                    assert.equal(false, isAuth);
+                    done();
+                });
+
+        })
+    })
+
+});
